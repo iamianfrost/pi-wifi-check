28c2eecebb6b5b6b4584cf826d283789cc6ec639
Ian Frost <47483887+iamianfrost@users.noreply.github.com>
2020-06-16 17:26:38 +0100

    Update wificheck.sh

--- wificheck.sh
+++ wificheck.sh
@@ -25,7 +25,7 @@ fi
 
 if [[ $(tail -n $failcount $loglocation | grep "No network" | wc -l) -eq $failcount ]];
 then
-  echo "`date '+%d/%m/%Y %H:%M:%S'` Last network restores didnt work, restarting " >> $loglocation
+  echo "`date '+%d/%m/%Y %H:%M:%S'` Last network restores failed, restarting " >> $loglocation
   sleep 1 
   reboot
 fi
